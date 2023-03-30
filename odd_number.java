8ea5f6ed25917c3914a11f2d11d61f03057f8470
Pranil Mali <pranildmali@gmail.com>
2023-03-30 12:34:24 +0530

    odd_number JAVA file

--- odd_number.java
+++ odd_number.java
@@ -5,11 +5,15 @@ public class odd_number {
 		public static void main(String[]args)
 		{
 			int s=11;
-			for (int f=1;f<=s;f++)
+			for (int f=1;f<=s;f++) // f = 1 to 11
 			{
 				if(f%2!=0)
 				{
 					System.out.println(f + " is an odd number");
+				}
+				else
+				{
+					System.out.println(f+ " is not an odd number");
 				}
 			}
 		}
